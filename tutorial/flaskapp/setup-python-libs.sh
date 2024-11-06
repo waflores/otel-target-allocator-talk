12f261cd96a5a240b0186c602b8634ac5c2f3f81
Will Flores <Will.Flores@intusurg.com>
2024-11-06 14:40:01 +0000

    Adding a setup python libs script for flaskapp.

---
+++ tutorial/flaskapp/setup-python-libs.sh
@@ -0,0 +1,7 @@
+# pip install opentelemetry-api
+# pip install opentelemetry-sdk
+# pip install opentelemetry-semantic-conventions
+pip install opentelemetry-distro opentelemetry-instrumentation opentelemetry-exporter-otlp flask
+opentelemetry-bootstrap -a install
+
+# See this page: https://opentelemetry.io/docs/languages/python/getting-started/
